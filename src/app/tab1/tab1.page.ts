d97a06a2d855c0812dcac64f5c4495aea560cfd8
chetanpal <chetanpal512@gmail.com>
2023-11-30 15:15:59 +0530

    added navigation and tried httpclient

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,10 @@ import { IonInput } from '@ionic/angular';
 import {  FormBuilder, FormGroup, } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { ModelcomponentComponent } from '../modelcomponent/modelcomponent.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -10,66 +14,97 @@ import { ModelcomponentComponent } from '../modelcomponent/modelcomponent.compon
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  modelinput:any;
-  testing:any;
-  @ViewChild('myInput') Input!: IonInput ;
-  ionicForm: FormGroup;
-  formdata:any;
-  constructor(public formBuilder: FormBuilder,private modalCtrl: ModalController) {
-    this.ionicForm = this.formBuilder.group({
-      productname:"",
-      productprice:"",
-      productquantity:"",
-      productdiscount:""
-    });
-  }
-  myfunction(){
-    console.log(this.Input.value);
-    this.testing=this.Input.value;
-    console.log(this.testing)
+    modelinput:any;
+    testing:any;
+    sendingdatatohttp:any;         
+    @ViewChild('myInput') Input!: IonInput ;
+    ionicForm: FormGroup;
+    formdata:any;
+    constructor(public formBuilder: FormBuilder,private modalCtrl: ModalController,private http:HttpClient,private navCtrl: NavController) {
+      this.ionicForm = this.formBuilder.group({
+        productname:"",
+        productprice:"",
+        productquantity:"",
+        productdiscount:""
+      });
 
-  }
-  mynewfunction(){
-    console.log(this.modelinput,"via ngmodel")
-  }
-  async openModal() {
-    console.log("inside submit form")
-    if (this.ionicForm.valid) {
-    let user = {
-      productname:this.ionicForm.value.productname,
-      productprice:this.ionicForm.value.productprice,
-      productquantity:this.ionicForm.value.productquantity,
-      productdiscount:this.ionicForm.value.productdiscount,
+    //   this.http.post('https://ionic-angular-project-4eae1-default-rtdb.firebaseio.com/offered-project.json',{price:"5000",product:"bulb",brand:"syska"}).subscribe((response)=>{
+    //   console.log("data stored successfully", response)
+    // },(error)=>{
+    //   console.error('Error storing data:', error);
+    // });
+
+    this.http.get('https://dummyjson.com/products').subscribe(response =>{
+      console.log(response)
+    })
+
     }
-    console.log(user,"user")
-    this.formdata=user;
-    console.log(this.formdata,"formdata")
-    const modal = await this.modalCtrl.create({
-      component: ModelcomponentComponent,
-      componentProps: { value: this.formdata },
-      // componentProps: { value: {names:"vishaka Mam",age:"don't know",departement:"ionic",Experience:"More than 2 years "} }
-    });
-    modal.present();
-  }
-  // referncefunction(inputEL:HTMLInputElement){
+    navigatetotab(){
+      this.navCtrl.navigateForward('/tabs/tab2');
+    }
+      myfunction(){
+        console.log(this.Input.value);
+        this.testing=this.Input.value;
+        console.log(this.testing)
 
-  // }
-  //   submitForm = () => {
-  //   console.log("inside submit form")
-  //   if (this.ionicForm.valid) {
-  //   let user = {
-  //     productname:this.ionicForm.value.productname,
-  //     productprice:this.ionicForm.value.productprice,
-  //     productquantity:this.ionicForm.value.productquantity,
-  //     productdiscount:this.ionicForm.value.productdiscount,
-  //   }
-  //   console.log(user,"user")
-  //   this.formdata=user;
-  //   console.log(this.formdata,"formdata")
-  //   console.log(JSON.stringify(user),"stringify")
-  //   }  
-  // };
+      }
+      mynewfunction(){
+        console.log(this.modelinput,"via ngmodel")
+      }
+      postDataToFirebase() {
+        console.log("inside postdatatofirebase")
+        const data = { price: '5000', product: 'mixer', brand: 'sujata'};
+    
+        this.http.post('https://your-firebase-project.firebaseio.com/offered-project.json', data)
+          .subscribe(
+            (response) => {
+              console.log('Data stored successfully', response);
+            },
+            (error) => {
+              console.error('Error storing data:', error);
+            }
+          );
+      }
+      async openModal() {
+        console.log("inside submit form")
+        if (this.ionicForm.valid) {
+          let user = {
+            productname:this.ionicForm.value.productname,
+            productprice:this.ionicForm.value.productprice,
+            productquantity:this.ionicForm.value.productquantity,
+            productdiscount:this.ionicForm.value.productdiscount,
+          }
+          console.log(user,"user")
+          this.formdata=user;
+          console.log(this.formdata,"formdata")
+          const modal = await this.modalCtrl.create({
+            component: ModelcomponentComponent,
+            componentProps: { value: this.formdata },
+            // componentProps: { value: {names:"vishaka Mam",age:"don't know",departement:"ionic",Experience:"More than 2 years "} }
+          });
+          modal.present();
+        }
+      }
+             
+    // referncefunction(inputEL:HTMLInputElement){
 
-}
+    // }
+    //   submitForm = () => {
+    //   console.log("inside submit form")
+    //   if (this.ionicForm.valid) {
+    //   let user = {
+    //     productname:this.ionicForm.value.productname,
+    //     productprice:this.ionicForm.value.productprice,
+    //     productquantity:this.ionicForm.value.productquantity,
+    //     productdiscount:this.ionicForm.value.productdiscount,
+    //   }
+    //   console.log(user,"user")
+    //   this.formdata=user;
+    //   console.log(this.formdata,"formdata")
+    //   console.log(JSON.stringify(user),"stringify")
+    //   }  
+    // };
+    
+             
 }
 
